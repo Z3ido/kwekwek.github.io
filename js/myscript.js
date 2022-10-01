84ba9fd7216b8038cae0e978e1205e2791bb6a8d
Z3ido <51947993+Z3ido@users.noreply.github.com>
2022-10-01 20:01:14 +0800

    Add files via upload

---
+++ js/myscript.js
@@ -0,0 +1,12 @@
+document.getElementById('calc').addEventListener('click',function(e)
+{
+    e.preventDefault();
+
+    var mynum1 = parseInt(document.getElementById('num1').value);
+    var mynum2 = parseInt(document.getElementById('num2').value);
+    var total = 0;
+
+    total = mynum1+mynum1;
+    alert('total');
+})
+
